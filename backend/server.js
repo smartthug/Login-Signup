333a4646199866160028a55e2d16e24971a4600b
smartthug <rahuldeva5555@gmail.com>
2025-05-15 11:37:00 +0530

    add cors

--- backend/server.js
+++ backend/server.js
@@ -3,11 +3,16 @@ import path from 'path';
 import { fileURLToPath } from 'url';
 import dotenv from 'dotenv';
 import './Database/mongodb.js';
+import cors from 'cors';
+
 const app = express();
 dotenv.config();
 const PORT = process.env.PORT || 3000;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+app.use(cors({
+  origin: process.env.CLIENT, // Replace with your frontend URL
+}));
 
 // Serve static files
 app.use(express.static(path.join(__dirname, '..', 'login-signup', 'dist')));
